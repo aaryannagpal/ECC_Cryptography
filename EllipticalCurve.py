25aa7d2dfa3eb6b01ac4c66f00cd8b0c4e95fcbd
Aaryan Nagpal <aaryannagpal65@gmail.com>
2022-10-05 23:10:53 +0530

    Will be used as Module File

--- EllipticalCurve.py
+++ EllipticalCurve.py
@@ -1,25 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Elliptic Curve Cryptography
-
-# ### Elliptic Curves
-
-# An elliptic curve over a field $K$ is a curve defined by an equation of the form:
-# 
-# \begin{equation}
-# \begin{split}
-#    y^2= x^3 + ax +b\\
-# \end{split}
-# \end{equation}
-# 
-# where $a, b \in K$ and $−16(4a^3 + 27b^2) \neq 0$
-
-# ### Importing necessary modules
-
-# In[31]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 inf = "Infinity"
@@ -27,24 +5,9 @@ inf = "Infinity"
 
 # ### Making some helper functions
 
-# In[32]:
-
 
 def Mod(x,p):
     return x%p
-
-
-# ##### We know if gcd(e, m) = 1, then $ed ≡ 1$ mod  $m$ has a unique solution:
-# $$gcd(e,m) = ex+my = 1$$
-# ##### From this we can calculate $d$
-# ##### To calculate $x$ and $y$, we extend the algorithm
-# ##### Let $e = qm + r$ such that $0 <= r < m$. Then
-# $$g = um + vr$$
-# $$= um + v(e − qm)$$
-# $$= ve + (u − qv)m$$
-# ##### Thus we can take $x = v$ and $y = u − qv$
-
-# In[33]:
 
 
 def extended_euclid(m,n):
@@ -63,18 +26,11 @@ def inv(s,p):
         else:
             return(x % p)
 
-
-# In[34]:
-
-
 # checking the inverse
 Mod(Mod(7,13)*inv(7,13),13)
 
 
 # ### Creating a class for Elliptic Curves
-
-# In[35]:
-
 
 class ECC:
     
@@ -184,74 +140,3 @@ class ECC:
                 X = self.power_mult(P,i)
                 product = self.add(X,product)
         return product
-
-
-# ### Creating an ECC
-
-# In[36]:
-
-
-EC = ECC(1,1, 17)
-
-Points = EC.Points()
-print(Points)
-
-
-# ### Binary Operations
-
-# #### Addition
-
-# If $P = (x_1, y_1) \in E$ then $−P = x_1,−y_1$ \
-# \
-# If $Q = (x_2, y_2) \in E, Q = −P,$ then
-# $$P + Q = (x_3, y_3)$$
-# 
-# where $x_3 = \lambda^2 −x_1 −x_2, y_3 = \lambda(x_1−x_3)−y_1 $ 
-# 
-# and
-# 
-# $$\lambda = (y_2 - y_1)/(x_2 - x_1)$$ 
-# if $P \neq Q$
-# $$\lambda = (3x_1^2+a)/2y_1$$ 
-# if $P = Q$
-
-# In[38]:
-
-
-EC.add(Points[2], Points[2])
-
-
-# In[39]:
-
-
-EC.pointDoubler(Points[2])
-
-
-# #### Multiplication
-
-# In[51]:
-
-
-EC.super_mult(Points[1],21)
-
-
-# ### Plotting it
-
-# In[41]:
-
-
-X = []
-for i in Points:
-    if i == inf:
-        pass
-    else:
-        X.append(i[0])
-Y = []
-for i in Points:
-    if i == inf:
-        pass
-    else:
-        Y.append(i[1])
-plt.scatter(X,Y)
-plt.grid()
-
